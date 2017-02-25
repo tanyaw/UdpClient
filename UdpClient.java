6e68d9ca7ed131b71fe2c8598fd0a44197a45594
micabalu <micabalu@cpp.edu>
2017-02-25 00:42:22 -0800

    stupid checksum

--- UdpClient.java
+++ UdpClient.java
@@ -9,11 +9,10 @@ public final class UdpClient{
         try (Socket socket = new Socket("codebank.xyz", 38005)) {
 
         	// Get address 
-			System.out.println("Connected to server.");  
 			String address = socket.getInetAddress().getHostAddress();
 			String[] split = address.split("\\.");
 
-			// Create input stream and input stream reader
+			// Create input stream 
 	        InputStream is = socket.getInputStream();
 
 			// Create output stream
@@ -23,25 +22,29 @@ public final class UdpClient{
 			int size = 2;
 			int udpSize = 2;
 			int udpLength = 0;
+			int length = 0;
 
 			//int udpPort = 0;
-			long udpPort = 0;;
+			long udpPort = 0;
 
 			// Needed to get randomized data for UDP source port field
    			Random ran = new Random();
-   			byte[] sourcePort = new byte[2];
 
 			for (int i = 0; i < 13; i++){
 				byte[] udpHeader = new byte[8];
 				byte[] udpData = new byte[udpSize];
+				byte[] pseudoheader = new byte[20];
 
 				// Handshaking step when i = 0
-				if (i == 0)
+				if (i == 0){
 					size = 4;
- 
+					length = size + 20;
+				}
+
+				else
+					length = size + 8 + 20;				
 
 				byte[] header = new byte[20];
-				int length = size + 20;
 
 				// Set header and version field
 				header[0] = 0b01000101;
@@ -84,6 +87,7 @@ public final class UdpClient{
 					header[j+16]  = (byte)Integer.parseInt(split[j]);
 				}
 
+
 				// Send to checksum function
 				short checksum = checksum(header);
 
@@ -99,28 +103,24 @@ public final class UdpClient{
 					data[2] = (byte)0b10111110;
 					data[3] = (byte)0b11101111;
 
+					// Reinitialize size (to be multiplied by 2 later on)
 					size = 1;
 				}
 
 				// Construct UDP packets for data
 				else{
-					//byte[] udpHeader = new byte[8];
-					//byte[] udpData = new byte[size];
 					ran.nextBytes(udpData);
 					udpLength = udpSize + 8;
-					byte[] pseudoheader = new byte[20];
 
 					/* Source port already initialized to 0 */
 
 					// Destination port obtained from handshake step
 					udpHeader[2] = (byte)((udpPort >> 8) & 0xFF); 
 					udpHeader[3] = (byte)(udpPort & 0xFF);  
-					
+		
 					// Length (UDP header + UDP data)
 					udpHeader[4] = (byte)((udpLength >> 8) & 0xFF); 
 					udpHeader[5] = (byte)(udpLength & 0xFF);  
-					System.out.println("udpheader[4] = " + udpHeader[4]);
-					System.out.println("udpheader[5] = " + udpHeader[5]);
 
 					// Checksum to be initialized later
 					udpHeader[6] = 0;
@@ -144,45 +144,39 @@ public final class UdpClient{
 					pseudoheader[10] = (byte)((udpLength >> 8) & 0xFF); 
 					pseudoheader[11] = (byte)(udpLength & 0xFF);  
 
-					/* Source port already initialized to 0 */
+					/* [12] [13] Source port already initialized to 0 */
 
-					// Destination port
-					pseudoheader[13] = udpHeader[2];
+					// Destination port [14] already 0
+					pseudoheader[15] = udpHeader[2];
 	
-					// Length
-					pseudoheader[14] = udpHeader[4];
+					// Length [16] already 0
+					pseudoheader[17] = udpHeader[4];
 
 					// Checksum for UDP packet and pseudoheader 
 					short udpChecksum = udpChecksum(udpHeader, pseudoheader, udpData);
 					udpHeader[6] = (byte)((int)(udpChecksum >> 8) & 0xFF); 
 					udpHeader[7] = (byte)((int)udpChecksum & 0xFF); 
 
-					pseudoheader[15] = (byte)((int)(udpChecksum >> 8) & 0xFF);
-
-					// Pseudo header [16] and after is UDP header + UDP data
-					/*for (int j = 0; j < 8; j++){
-						pseudoheader[16 + j] = udpHeader[j]; 
-					}
-
-					for (int j = 0; j < udpLength; j++){
-						pseudoheader[24+j] = udpData[j];
-					}*/
+					pseudoheader[18] = (byte)((int)(udpChecksum >> 8) & 0xFF);
+					pseudoheader[19] = (byte)((int)udpChecksum & 0xFF); 
 
 					// Do I compute udpchecksum here??
 					udpSize *= 2;
 				}
 
-				// Send packets to the server with each packet number
+				// Send the IPV4 header
 				os.write(header);
 
+				// Send handshake
 				if (i == 0){
 					os.write(data);
 					System.out.print("Handshake response: 0x");
 				}
+
+				// Otherwise, send UDP header + UDP data
 				else{
 					os.write(udpHeader);
 					os.write(udpData);
-					System.out.println("udp data size = " + udpData.length);
 					System.out.println("Sending packet with " + size + " bytes of data");
 					System.out.print("Response: 0x");
 				}
@@ -196,21 +190,14 @@ public final class UdpClient{
 				int b1;
 				int b2;
 
-
 				// Get port number
 				if (i == 0){
-					b1 = is.read();
-
-	        		// Read from the input stream a second time
+	        		// Read from the input stream and concatenate 
+	        		b1 = is.read();
 	        		b2 = is.read();
-
 	      			udpPort += (long)(((b1 << 8) & 0xFF00) | (b2 & 0xFF));  
 	    
-	        		// Concatenate 
 	        		System.out.println("Port number received: " + udpPort);
-					//String s2 = String.format("%8s", Integer.toBinaryString(portNum[1] & 0xFF)).replace(' ', '0');
-					//System.out.println(s2);
-
 				}
 
 				/*//Print binary output of IPv4 pakcet headers
@@ -264,8 +251,8 @@ public final class UdpClient{
   	}
 
    	public static short udpChecksum(byte[] pseudoheader, byte[] udpHeader, byte[] data){
-    	// The 16 bit number sum
-    	int sum = 0;
+    	// The 32 bit number sum
+    	long sum = 0;
 
     	sum = sum(pseudoheader) + sum(udpHeader) + sum(data);
 	    
@@ -273,8 +260,8 @@ public final class UdpClient{
 	    return (short)~(sum & 0xFFFF);
   	} 	
 
-  	public static int sum(byte[] b){
-  		int sum = 0;
+  	public static long sum(byte[] b){
+  		long sum = 0;
 
   		for (int i = 0; i < b.length-1; i+=2){
 	     // Form a 16-bit number out of the two bytes and add to the sum
